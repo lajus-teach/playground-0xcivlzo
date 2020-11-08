65fab85548cb45297974f0f4ffd5d680b67c13b3
Jonathan Lajus <nomail@noname.com>
2020-11-08 22:27:40 +0100

    Update test_moyenne.py

--- python-project/test_moyenne.py
+++ python-project/test_moyenne.py
@@ -1,4 +1,5 @@
 ﻿import moyenne
+import math
 
 def send_msg(channel, msg):
     print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
@@ -10,14 +11,14 @@ def success():
 
 def fail():
     print("TECHIO> success false")
-    
+
 
 def test_count_all_stars():
     try:
         m1 = moyenne.moyenne([1.2, 3.2, 3.6, 4.2, 5.7])
-        assert m1 == 3.58, "Running moyenne([1.2, 3.2, 3.6, 4.2, 5.7])... Expected 3.58, got {}".format(m1)
+        assert math.isclose(m1, 3.58), "Running moyenne([1.2, 3.2, 3.6, 4.2, 5.7])... Expected 3.58, got {}".format(m1)
         m2 = moyenne.moyenne([1, 4, 12, 32])
-        assert m2 == 12.25, "Running moyenne([1, 4, 12, 32])... Expected 12.25, got {}".format(m2)
+        assert math.isclose(m2, 12.25), "Running moyenne([1, 4, 12, 32])... Expected 12.25, got {}".format(m2)
         success()
 
         send_msg("Bien joué !", "")
